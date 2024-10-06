07749c3e97d455a12a754db1e337dbfa3f76fd49
Tianyu Du <tianyudu@stanford.edu>
2024-10-06 15:41:52 -0700

    update code for running CAREER https://github.com/gsbDBI/career_transformer/issues/92

--- career_v1.sh
+++ career_v1.sh
@@ -1,39 +1,67 @@
+# This script runs the CAREER model on datasets with random seed fixed.
+
+# ======================================================================================================================
+# activate the virtual environment, please refer to CAREER's README for more details of the dependencies.
+# ======================================================================================================================
 source /oak/stanford/groups/athey/tianyudu/Development/career_v1_replication/career_v1/bin/activate
-
-
-# cd /oak/stanford/groups/athey/tianyudu/Development/career_v1_replication/career-code/fairseq
 # use the BINARY_DATA_DIR/SAVE_DIR/LOG_DIR on SCRATCH for better disk IO.
-cd /scratch/users/tianyudu/career-code/fairseq
-
+# activate slurm modules, you do not need to do this if you are running this on your local machine.
 ml py-pytorch/1.8.1_py39
 ml py-numpy/1.20.3_py39
 
+# ======================================================================================================================
+# set the data directories
+# ======================================================================================================================
+# set the path where you download the career-code repo.
+REPO_PATH="/scratch/users/tianyudu/career-code"
+# set the dataset name, one of "NLSY79", "NLSY97", "PSID79", we make it depends on the SLURM_ARRAY_TASK_ID to run
+# different datasets in parallel.
+if [ $SLURM_ARRAY_TASK_ID == 0 ]; then
+  DATASET_NAME="NLSY79"
+elif [ $SLURM_ARRAY_TASK_ID == 1 ]; then
+  DATASET_NAME="NLSY97"
+elif [ $SLURM_ARRAY_TASK_ID == 2 ]; then
+  DATASET_NAME="PSID79"
+fi
 
+cd ${REPO_PATH}/fairseq
+
+# set directories.
 RESUME_DATA_DIR=/oak/stanford/groups/athey/laborai/Zippia/fairseq_input_unified_one_job_year_without_unemployment_nilf_student_no_skipped_jobs_occ1990dd
-du -hs ${RESUME_DATA_DIR}
+SURVEY_DATA_DIR=/oak/stanford/groups/athey/LABOR_LLM_data/career_format_data/${DATASET_NAME}
+BINARY_DATA_DIR=${REPO_PATH}/data_bin_${DATASET_NAME}
 
-SURVEY_DATA_DIR=/oak/stanford/groups/athey/career_transformer_data/CAREER_v1_raw_data/PSID79
+# fix the random seed.
+SEED=1
+SAVE_DIR=${REPO_PATH}/checkpoints_${DATASET_NAME}_seed_${SEED}
+LOG_DIR=${REPO_PATH}/logs_${DATASET_NAME}_seed_${SEED}
+SURVEY_DATASET_NAME=${DATASET_NAME}
 
-
-BINARY_DATA_DIR=/scratch/users/tianyudu/career-code/fairseq/fairseq/data-bin
-SAVE_DIR=/scratch/users/tianyudu/career-code/fairseq/fairseq/checkpoints
-LOG_DIR=/scratch/users/tianyudu/career-code/fairseq/fairseq/logs
-SURVEY_DATASET_NAME=/scratch/users/tianyudu/career-code/fairseq/psid79
+echo "DATASET_NAME: ${DATASET_NAME}"
+echo "BINARY_DATA_DIR: ${BINARY_DATA_DIR}"
+echo "SAVE_DIR: ${SAVE_DIR}"
+echo "LOG_DIR: ${LOG_DIR}"
+echo "SURVEY_DATASET_NAME: ${SURVEY_DATASET_NAME}"
 
 mkdir ${BINARY_DATA_DIR}
 mkdir ${SAVE_DIR}
 mkdir ${LOG_DIR}
 
-
+# this would take around 30 minutes.
 sh preprocess/preprocess_transfer_learning_datasets.sh \
   -r $RESUME_DATA_DIR -s $SURVEY_DATA_DIR \
   -b $BINARY_DATA_DIR -n $SURVEY_DATASET_NAME
 
 
+du -hs ${BINARY_DATA_DIR}
+du -hs ${SAVE_DIR}
+du -hs ${LOG_DIR}
 
+# this could take around 12 hours.
 fairseq-train --task occupation_modeling \
   $BINARY_DATA_DIR/resume-pretraining \
   --arch career \
+  --seed=${SEED} \
   --optimizer adam --adam-betas '(0.9, 0.98)' --weight-decay 0.01 \
   --clip-norm 0.0 \
   --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
@@ -46,10 +74,13 @@ fairseq-train --task occupation_modeling \
   --fp16 --two-stage \
   --include-year --include-education --include-location
 
+# if you observe a numeric error, it is often the case that you are over-fitting the fine-tuning set.
+# This means you are all set because the script will save the best checkpoint.
 fairseq-train --task occupation_modeling \
   $BINARY_DATA_DIR/$SURVEY_DATASET_NAME \
   --finetune-from-model $SAVE_DIR/resume-pretraining/career/checkpoint_best.pt \
   --arch career \
+  --seed=${SEED} \
   --optimizer adam --adam-betas '(0.9, 0.98)' --weight-decay 0.01 \
   --clip-norm 0.0 \
   --lr 0.0001 --lr-scheduler inverse_sqrt --warmup-updates 500 \
@@ -67,9 +98,9 @@ fairseq-train --task occupation_modeling \
 # Might encounter the following error:
 # FloatingPointError: Minimum loss scale reached (0.0001). Your loss is probably exploding. Try lowering the learning rate, using gradient clipping or increasing the batch size.
 
-cd ../analysis
-python compute_survey_data_perplexity.py --model-name career \
-  --binary-data-dir $BINARY_DATA_DIR \
-  --save-dir $SAVE_DIR \
-  --dataset-name $SURVEY_DATASET_NAME
+# cd ../analysis
+# python compute_survey_data_perplexity.py --model-name career \
+#   --binary-data-dir $BINARY_DATA_DIR \
+#   --save-dir $SAVE_DIR \
+#   --dataset-name $SURVEY_DATASET_NAME
 
